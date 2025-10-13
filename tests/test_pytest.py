442edc31523e62f96035ecb9cac6b11647deb032
Aleksandr <nc6@bk.ru>
2025-10-13 12:59:07 +0300

    Интеграция Pytest и Playwright

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+def test_user_login():
+    print("Testing user login 0")
+
+
+class TestUserLogin:
+    def test_1(self):
+        print("Testing user login 1")
+
+    def test_2(self):
+        print("Testing user login 2")
