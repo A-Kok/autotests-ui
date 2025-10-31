7f686573becef24eee2c203335ecae438a32f7ff
Aleksandr <nc6@bk.ru>
2025-10-31 11:43:19 +0300

    Интеграция Allure в автотесты

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,37 +1,55 @@
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
 import pytest
+import allure
+
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from allure_commons.types import Severity
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
 class TestCourses:
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
-        courses_list_page.navigate('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+        courses_list_page.navigate(
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
+        )
 
-        courses_list_page.navbar.check_visible('username')
+        courses_list_page.navbar.check_visible("username")
         courses_list_page.sidebar.check_visible()
 
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
-    def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
+    def test_create_course(
+        self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage
+    ):
         create_course_page.navigate(
-            'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
+        )
 
         create_course_page.create_course_toolbar_view.check_visible()
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
         create_course_page.create_course_form.check_visible(
-            title="",
-            estimated_time="",
-            description="",
-            max_score="0",
-            min_score="0"
+            title="", estimated_time="", description="", max_score="0", min_score="0"
         )
         create_course_page.create_course_exercises_toolbar_view.check_visible()
         create_course_page.check_visible_exercises_empty_view()
 
-        create_course_page.image_upload_widget.upload_preview_image(file="./testdata/files/image.png")
+        create_course_page.image_upload_widget.upload_preview_image(
+            file="./testdata/files/image.png"
+        )
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
         create_course_page.create_course_form.fill(
             title="Playwright",
