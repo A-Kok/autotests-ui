bb6086481e1226f88491583e8bb920dab7216dc3
Aleksandr <nc6@bk.ru>
2025-11-11 16:04:21 +0300

    + обавляем информацию о переменных окружения в Allure отчет

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,20 +2,29 @@ from playwright.sync_api import Page, expect
 from typing import Pattern
 import allure
 
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
+
 
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
     def navigate(self, url: str):
-        with allure.step(f'Opening thee url: {url}'):
+        step = f'Opening thee url: {url}'
+        with allure.step(step):
+            logger.info(step)
             self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
+        step = f'Reloading the page with url "{self.page.url}"'
         with allure.step(f'Reloading the page with url "{self.page.url}"'):
+            logger.info(step)
             self.page.reload(wait_until='networkidle')
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(
-                f'Checking the current url "{self.page.url}" matches the expected pattern "{expected_url.pattern}"'):
+        step = f'Checking the current url "{self.page.url}" matches the expected pattern "{expected_url.pattern}"'
+        with allure.step(step):
+            logger.info(step)
             expect(self.page).to_have_url(expected_url)
